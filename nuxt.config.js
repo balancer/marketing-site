847ab707cb20f1f67aae32a255615457081d11d0
Gareth Fuller <gareth.a.f@gmail.com>
2021-10-27 15:10:32 +0100

    Add sitemap config

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,5 +64,9 @@ export default {
 
   sentry: {
     dsn: 'https://991f8a3901034e0db96f0ff7bf10d24e@o574636.ingest.sentry.io/5818459'
+  },
+
+  sitemap: {
+    hostname: 'https://balancer.fi',
   }
 }
