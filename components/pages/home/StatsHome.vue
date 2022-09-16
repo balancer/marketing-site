fc6f12dc37f06fc467f3dec4c02d0f177ccd9ebf
pkattera <kattera@gmail.com>
2022-09-16 17:40:20 +0100

    Updated stats

--- components/pages/home/StatsHome.vue
+++ components/pages/home/StatsHome.vue
@@ -1,14 +1,15 @@
 <template>
   <section class="p-4 bg-white">
-    <div class="content-container">
+    <div class="content-container pt-8 pb-8 md:pb-20 md:pt-2 lg:px-8">
       <transition name="stats" appear>
-        <div class="grid grid-cols-2 md:grid-cols-4 pt-8 pb-8 md:pb-16 md:pt-2 lg:px-8">        
-          <Stat label="Trade vol (7d)" stat="$716m" />
-          <Stat label="Total fees earned" stat="$72.3m" />
-          <Stat label="Total liquidity" stat="$3.4b" />
-          <Stat label="Liquidity Providers" stat="17.5k" />        
-        </div>
+        <div class="grid grid-cols-2 md:grid-cols-4">                  
+          <Stat label="Trade vol (7d)*" stat="$462m" />          
+          <Stat label="Total liquidity*" stat="$1.05b" />
+          <Stat label="Liquidity Providers*" stat="21.7k" />        
+          <Stat label="Total pools*" stat="3,621" />          
+        </div>        
       </transition>
+      <p class="md:text-center pt-6 md:pt-8 text-gray-500 text-sm">* Stats as at 16 September 2022</p>
     </div>
   </section>
 </template>
