b727330e9979500b3ab7a269d2b13fa6c1abd2f3
pkattera <kattera@gmail.com>
2023-01-18 12:50:52 +0000

    Made the site document driven for Nuxt Content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     ],
     content: {
         // https://content.nuxtjs.org/api/configuration
+        documentDriven: true,
     },
     components: {
         dirs: [
